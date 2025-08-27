907b002eda7ae974a771e8d7820eca4b168c73b6
Miron Foerster <miron.foerster@gmx.de>
2025-08-27 15:25:33 +0200

    allow multi client debug

--- run_debug.bat
+++ run_debug.bat
@@ -4,10 +4,29 @@ setlocal
 :: Path to your project
 set PROJECT="."
 
+:: Number of clients (default 1)
+set NCLIENTS=1
+
+:: Check if --n-clients argument was passed
+for %%A in (%*) do (
+    if "%%~A"=="--n-clients" (
+        set NEXT_IS_N=1
+    ) else if defined NEXT_IS_N (
+        set NCLIENTS=%%~A
+        set NEXT_IS_N=
+    )
+)
+
 :: Start server (headless, in new terminal)
 start "Server" godot4console --server --headless --path %PROJECT%
 
-:: Start client (normal, in new terminal)
-start "Client" godot4console --path %PROJECT%
+:: Start clients
+set /a COUNTER=1
+:client_loop
+if %COUNTER% leq %NCLIENTS% (
+    start "Client %COUNTER%" godot4console --path %PROJECT%
+    set /a COUNTER+=1
+    goto client_loop
+)
 
 endlocal
