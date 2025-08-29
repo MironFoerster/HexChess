82265e71633d9e8aa595991d23bb5d2a0a70f2c8
Miron Foerster <miron.foerster@gmx.de>
2025-08-29 15:32:01 +0200

    move tilemap to session

---
+++ scripts/session.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var current_turn = -1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+#func ripple (tile: Node3D) -> void:
+	#for distance in range(map_radius*2 +1):
+		#for t in $Tiles.get_children():
+			#if t.coordinates.distance_to(tile.coordinates) == distance:
+				#t.toggle_highlight()
+				##t.get_node("AnimationPlayer").play("bump")
+		#await get_tree().create_timer(0.06).timeout
+#
+#func generate_map(radius: int):
+	#for q in range(-radius, radius+1):
+		#for r in range(-radius, radius+1):
+			#for s in range(-radius, radius+1):
+				#if q + r + s == 0:
+					#var tile_instance = TileScene.instantiate()
+					#tile_instance.call_deferred("set_name", "Tile")
+					#$Tiles.add_child(tile_instance)
+					#tile_instance.initialize(CubeCoordinates.new(q, r, s))
