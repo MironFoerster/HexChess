76c804872d0744c914309fbc4d4a26f4fe26a0f2
Miron Foerster <miron.foerster@gmx.de>
2025-09-01 15:35:07 +0200

    implement all identification functionalities, not tested

--- scripts/autoloads/global_networking.gd
+++ scripts/autoloads/global_networking.gd
@@ -2,17 +2,20 @@ extends Node
 
 var database := preload("res://scripts/server/database_controller.gd").new()
 
+var local_player: PlayerData
+
 # only for server
 var connected_players: Array[int] = [3]
-var registered_players: Dictionary = {3: PlayerData.new("bob")}  # Dictionary[int, PLayerData]
+var identified_players: Dictionary[int, PlayerData] = {3: PlayerData.new("bob")}
 
 # contains max one session on clients
 var sessions: Array[SessionData] = [SessionData.new("Session2", "private", 3, [3,4,5,6], {}, "123456")]
 
 # These signals can be connected to by a UI lobby scene or the game scene.
-signal player_connected(peer_id, player_info)
-signal player_disconnected(peer_id)
+signal player_connected(peer_id: int, player_info)
+signal player_disconnected(peer_id: int)
 signal server_disconnected
+signal ident_processed(success: bool)
 
 var server_ip = "172.20.32.1"
 var port = 3000
@@ -43,7 +46,7 @@ func start_server():
 	print("Server started on port %d" % port)
 
 	
-func connect_to_server(on_success: Callable, on_failure: Callable):
+func _connect_to_server(on_success: Callable, on_failure: Callable):
 	var peer = ENetMultiplayerPeer.new()
 	var err = peer.create_client(server_ip, port)
 	if err != OK:
@@ -54,6 +57,108 @@ func connect_to_server(on_success: Callable, on_failure: Callable):
 	_connect_success_callback = on_success
 	_connect_failure_callback = on_failure
 
+
+### LOGIN ###
+func login_user(username: String, password: String):
+	if multiplayer.multiplayer_peer: #TODO: make sure is set to null on disconnect
+		# call _login_user rpc on server
+		_login_user.rpc_id(1, username, password)
+	else:
+		# first connect to server before rpc call
+		_connect_to_server(func(): _login_user.rpc_id(1, username, password),
+						func(): ident_processed.emit(false))
+
+@rpc("any_peer", "call_remote", "reliable")  # server
+func _login_user(username: String, password: String):
+	var sender_id = multiplayer.get_remote_sender_id()
+	if database.validate_password(username, password):
+		var player = PlayerData.new(username, false, database.get_user_rank(username))
+		
+		identified_players[sender_id] = player
+		_login_processed.rpc_id(sender_id, true, player.serialize())
+	else:
+		_login_processed.rpc_id(sender_id, false)
+		
+@rpc("authority", "call_remote", "reliable")  # client
+func _login_processed(success: bool, serialized_player: Dictionary[String, Variant] = {}):
+	local_player = PlayerData.deserialize(serialized_player)
+	ident_processed.emit(success)
+
+
+### NICKNAME ###
+func nickname_user(nickname: String):
+	if multiplayer.multiplayer_peer: #TODO: make sure is set to null on disconnect
+		# call _nickname_user rpc on server
+		_nickname_user.rpc_id(1, nickname)
+	else:
+		# first connect to server before rpc call
+		_connect_to_server(func(): _nickname_user.rpc_id(1, nickname),
+						func(): ident_processed.emit(false))
+
+@rpc("any_peer", "call_remote", "reliable")  # server
+func _nickname_user(nickname: String):
+	var sender_id = multiplayer.get_remote_sender_id()
+	var player = PlayerData.new(nickname, true)
+	
+	identified_players[sender_id] = player
+	_nickname_processed.rpc_id(sender_id, true, player.serialize())
+
+
+@rpc("authority", "call_remote", "reliable")  # client
+func _nickname_processed(success: bool, serialized_player: Dictionary[String, Variant] = {}):
+	local_player = PlayerData.deserialize(serialized_player)
+	ident_processed.emit(success)
+
+
+### REGISTER ###
+func register_user(username: String, password: String):
+	if multiplayer.multiplayer_peer: #TODO: make sure is set to null on disconnect
+		# call _register_user rpc on server
+		_register_user.rpc_id(1, username, password)
+	else:
+		# first connect to server before rpc call
+		_connect_to_server(func(): _register_user.rpc_id(1, username, password),
+						func(): ident_processed.emit(false))
+
+@rpc("any_peer", "call_remote", "reliable")  # server
+func _register_user(username: String, password: String):
+	var sender_id = multiplayer.get_remote_sender_id()
+	database.add_user(username, password)
+	var player = PlayerData.new(username, false, database.get_user_rank(username))
+	
+	identified_players[sender_id] = player
+	_register_processed.rpc_id(sender_id, true, player.serialize())
+
+@rpc("authority", "call_remote", "reliable")  # client
+func _register_processed(success: bool, serialized_player: Dictionary[String, Variant] = {}):
+	local_player = PlayerData.deserialize(serialized_player)
+	ident_processed.emit(success)
+
+
+### JOIN PARTY ###
+func check_join_code(code: String):
+	_check_join_code.rpc_id(1, code)
+
+# server
+@rpc("any_peer", "call_remote", "reliable")
+func _check_join_code(code: String):
+	for session in sessions:
+		if code == session.game_code:
+			#TODO
+			pass
+			#_player_joined.rpc(connected_players.find(func(p): return p.id == multiplayer.get_remote_sender_id()))
+	
+#@rpc("any_peer", "call_remote", "reliable")
+#func _player_joined(player: PlayerData):
+	##TODO handle self sifferently than others
+	#joined_players[multiplayer.get_remote_sender_id()] = player
+
+
+
+
+
+
+### MULTIPLAYER API HANDLERS ###
 func _on_player_connected(id):
 	if multiplayer.is_server():
 		print("A client connected.")
@@ -66,7 +171,7 @@ func _on_player_disconnected(id):
 	if multiplayer.is_server():
 		print("A client disconnected.")
 		connected_players.erase(id)
-		registered_players.erase(id)
+		identified_players.erase(id)
 	else:
 		print("Another player disconnected.")
 	#players.erase(id)
@@ -88,27 +193,7 @@ func _on_server_disconnected():
 	multiplayer.multiplayer_peer = null
 	print("Server disconnected...trying to reconnect...")
 	server_disconnected.emit() #TODO: make sure every place that listenes to networking signals is never instanciated on the server
-	connect_to_server(_connect_success_callback, _connect_failure_callback) #TODO: maybe different callbacks?
-
-
-func check_join_code(code: String):
-	_check_join_code.rpc_id(1, code)
-	
-@rpc("any_peer", "call_remote", "reliable")
-func _check_join_code(code: String):
-	for session in sessions:
-		if code == session.game_code:
-			#TODO
-			pass
-			#_player_joined.rpc(connected_players.find(func(p): return p.id == multiplayer.get_remote_sender_id()))
-	
-#@rpc("any_peer", "call_remote", "reliable")
-#func _player_joined(player: PlayerData):
-	##TODO handle self sifferently than others
-	#joined_players[multiplayer.get_remote_sender_id()] = player
-
-
-
+	_connect_to_server(_connect_success_callback, _connect_failure_callback) #TODO: maybe different callbacks?
 
 
 ## LEFTOVERS ##
--- scripts/ui/login_page.gd
+++ scripts/ui/login_page.gd
@@ -1,8 +1,18 @@
 extends Control
 
+@onready var nickname = $PlayOnlineContainer/LoginTabContainer/NicknameContainer/HBoxContainer/NicknameLineEdit
+@onready var login_username = $PlayOnlineContainer/LoginTabContainer/LoginContainer/UsernameLineEdit
+@onready var login_password = $PlayOnlineContainer/LoginTabContainer/LoginContainer/PasswordLineEdit
+@onready var register_username = $PlayOnlineContainer/LoginTabContainer/RegisterContainer/UsernameLineEdit
+@onready var register_password = $PlayOnlineContainer/LoginTabContainer/RegisterContainer/PasswordLineEdit
+@onready var register_password_repeat = $PlayOnlineContainer/LoginTabContainer/RegisterContainer/PasswordRepeatLineEdit
+
 signal request_page_change(page_name)
 
+func _ready() -> void:
+	GlobalNetworking.ident_processed.connect(_on_ident_processed)
 
+## TAB NAVIGATION
 func _on_nickname_link_button_pressed() -> void:
 	$PlayOnlineContainer/LoginTabContainer.current_tab = 0
 
@@ -11,3 +21,28 @@ func _on_login_link_button_pressed() -> void:
 
 func _on_register_link_button_pressed() -> void:
 	$PlayOnlineContainer/LoginTabContainer.current_tab = 2
+
+## SUBMIT HANDLERS
+func _on_nickname_submit_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_login_submit_button_pressed() -> void:
+	GlobalNetworking.login_user(login_username.text, login_password.text)
+
+func _on_ident_processed(success: bool):
+	_clear_all_inputs()
+	if success:
+		request_page_change.emit("home")
+
+func _on_register_submit_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+## UTIL
+func _clear_all_inputs():
+	nickname.clear()
+	login_username.clear()
+	login_password.clear()
+	register_username.clear()
+	register_password.clear()
+	register_password_repeat.clear()
