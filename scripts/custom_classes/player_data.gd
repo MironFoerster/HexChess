09cd3f1db6a08e8a259a0263954e6754547a4639
Miron Foerster <miron.foerster@gmx.de>
2025-09-01 15:34:21 +0200

    make player and session data serializable

--- scripts/custom_classes/player_data.gd
+++ scripts/custom_classes/player_data.gd
@@ -1,9 +1,25 @@
-extends Resource
+extends Object
 class_name PlayerData
 
-var nickname: String
+var name: String
+var is_temp: bool
 var rank: int
 
-func _init(_nickname: String, _rank: int = 0):
-	nickname = _nickname
+func _init(_name: String = "", _is_temp: bool = true, _rank: int = 0):
+	name = _name
+	is_temp = _is_temp
 	rank = _rank
+
+func serialize() -> Dictionary[String, Variant]:
+	return {
+		"name": name,
+		"is_temp": is_temp,
+		"rank": rank
+	}
+
+static func deserialize(data: Dictionary[String, Variant]) -> PlayerData:
+	var player = PlayerData.new()
+	player.name = data.get("name", "")
+	player.is_temp = data.get("is_temp", false)
+	player.rank = data.get("rank", 0)
+	return player
--- scripts/custom_classes/session_data.gd
+++ scripts/custom_classes/session_data.gd
@@ -1,17 +1,37 @@
-extends Resource
+extends Object
 class_name SessionData
 
 var node_name: String
 var type: String # private / public
 var admin_id: int
 var player_ids: Array[int]  # used by server
-var players: Dictionary  #used by clients
+var players: Dictionary  # used by clients
 var game_code: String
 
-func _init(_node_name: String, _type: String, _admin_id: int, _player_ids: Array[int], _players: Dictionary, _game_code: String) -> void:
+func _init(_node_name: String = "", _type: String = "public", _admin_id: int = -1, _player_ids: Array[int] = [], _players: Dictionary = {}, _game_code: String = "") -> void:
 	node_name = _node_name
 	type = _type
 	admin_id = _admin_id
 	player_ids = _player_ids
 	players = _players
 	game_code = _game_code
+
+func serialize() -> Dictionary[String, Variant]:
+	return {
+		"node_name": node_name,
+		"type": type,
+		"admin_id": admin_id,
+		"player_ids": player_ids,
+		"players": players,
+		"game_code": game_code
+	}
+
+static func deserialize(data: Dictionary[String, Variant]) -> SessionData:
+	var session = SessionData.new()
+	session.node_name = data.get("node_name", "")
+	session.type = data.get("type", "public")
+	session.admin_id = data.get("admin_id", -1)
+	session.player_ids = data.get("player_ids", [])
+	session.players = data.get("players", {})
+	session.game_code = data.get("game_code", "")
+	return session
