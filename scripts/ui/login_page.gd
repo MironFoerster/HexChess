6a9090a14a3b64edc533e0ef661909a170e4990d
Miron Foerster <miron.foerster@gmx.de>
2025-09-03 21:19:15 +0200

    connect login buttons to networking calls

--- scripts/ui/login_page.gd
+++ scripts/ui/login_page.gd
@@ -24,18 +24,24 @@ func _on_register_link_button_pressed() -> void:
 
 ## SUBMIT HANDLERS
 func _on_nickname_submit_button_pressed() -> void:
-	pass # Replace with function body.
+	GlobalNetworking.nickname_user(nickname.text)
 
 func _on_login_submit_button_pressed() -> void:
 	GlobalNetworking.login_user(login_username.text, login_password.text)
 
+func _on_register_submit_button_pressed() -> void:
+	if register_password.text == register_password_repeat.text:
+		GlobalNetworking.register_user(register_username.text, register_password.text)
+	else:
+		print("Passwords dont match.")
+		_clear_all_inputs()
+
+## HANDLE IDENTIFICATION RESULT
 func _on_ident_processed(success: bool):
 	_clear_all_inputs()
 	if success:
 		request_page_change.emit("home")
 
-func _on_register_submit_button_pressed() -> void:
-	pass # Replace with function body.
 
 
 ## UTIL
